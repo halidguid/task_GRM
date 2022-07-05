b715a508ba46a52809622f9db52cade52424446a
halidguid <halidhosi@gmail.com>
2022-07-05 21:19:09 +0200

    taskGRM

--- task_grm/src/Task.js
+++ task_grm/src/Task.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { Button, Table, Modal, Form } from "react-bootstrap";
-import { combinations } from "itertools";
 
 const list = [
   {
